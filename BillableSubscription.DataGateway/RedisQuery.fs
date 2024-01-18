04561a1ed768cc71d3551f21c76a716fb7f30fa9
Scott Nimrod <scott.nimrod@bizmonger.net>
2024-01-18 11:40:49 -0500

    Misc.

--- BillableSubscription.DataGateway/RedisQuery.fs
+++ BillableSubscription.DataGateway/RedisQuery.fs
@@ -1,28 +1,36 @@
 ﻿namespace BeachMobile.BillableSubscription.DataGateway.Redis
 
 open System
-open BeachMobile.BillableSubscription.Operations
+open Newtonsoft.Json
 open StackExchange.Redis
 open Configuration
+open BeachMobile.BillableSubscription.Operations
+open BeachMobile.BillableSubscription.Language
 
 module Get =
 
-    let subscription : GetRegistrationStatus =
+    let status : GetRegistrationStatus =
 
         fun v -> async { 
 
             let! connection = ConnectionMultiplexer.ConnectAsync(ConnectionString.Instance) |> Async.AwaitTask
             let cache = connection.GetDatabase()
-            let result = cache.StringGet("someKey")
-            return Error ""
+            let response = cache.StringGet(v.Registration.id)
+            
+            match response.HasValue with
+            | false -> return Ok None
+            | true  -> return Ok (response |> JsonConvert.DeserializeObject<RegistrationStatus> |> Some)
         }
 
     let paymentHistory : GetPaymentHistory = 
 
-        fun v -> async {
+        fun v -> async { 
 
             let! connection = ConnectionMultiplexer.ConnectAsync(ConnectionString.Instance) |> Async.AwaitTask
             let cache = connection.GetDatabase()
-            let result = cache.StringGet("someKey")
-            return Error ""
+            let response = cache.StringGet(v)
+            
+            match response.HasValue with
+            | false -> return Ok []
+            | true  -> return Ok (response |> JsonConvert.DeserializeObject<SuccessfulPayment seq>)
         }
